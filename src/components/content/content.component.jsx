5e82d9a73b89552dc0ea0ddc4f58b528ab0596ba
dkp1903 <dushyantpathak6789@gmail.com>
2020-08-25 00:16:18 +0530

    add finale1

--- src/components/content/content.component.jsx
+++ src/components/content/content.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Papa from 'papaparse';
-
+import p from 'papaparse';
 import InfoBox from '../info-box/info-box.component';
 import ChartDisplay from '../chart-display/chart-display.component';
+import image from '../../img/ipl-trophy.jpg';
 
 import './content.styles.css';
 
@@ -34,61 +34,24 @@ class Content extends React.Component {
 
     // Converting CSV file to JSON data
     getChartData = () => {
-        Papa.parse('./ipl-csv-dataset/Match.csv', {
-            header: true,
-            download: true,
-            dynamicTyping: true,
-            complete: this.saveMatch
-        });
-        Papa.parse('./ipl-csv-dataset/matches.csv', {
+        
+        p.parse('./ipl-csv-dataset/matches.csv', {
             header: true,
             download: true,
             dynamicTyping: true,
             complete: this.getData
         });
-        Papa.parse('./ipl-csv-dataset/Player.csv', {
-            header: true,
-            download: true,
-            dynamicTyping: true,
-            complete: this.savePlayer
-        });
-        Papa.parse('./ipl-csv-dataset/Ball_by_Ball.csv', {
-            header: true,
-            download: true,
-            dynamicTyping: true,
-            complete: this.saveBallByBall
-        });
-        /*Papa.parse('./ipl-csv-dataset/Season.csv', {header: true, download: true, dynamicTyping: true, complete: this.saveSeason});
-        Papa.parse('./ipl-csv-dataset/Team.csv', {header: true, download: true, dynamicTyping: true, complete: this.saveTeam});
-        Papa.parse('./ipl-csv-dataset/Player_Match.csv', {header: true, download: true, dynamicTyping: true, complete: this.savePlayerMatch});*/
     }
 
     // Saving JSON data to state and calling associated functions to process data
-    saveMatch = (result) => {
-        this.setState({ rawMatch: result });
-        this.dataForChart1();
-        
-        
-        
-    }
-    savePlayer = (result) => {
-        this.setState({ rawPlayer: result });
-        this.dataForChart4();
-        this.dataForChart5();
-        this.dataForChart6();
-    }
-    saveBallByBall = (result) => {
-        this.setState({ rawBallByBall: result });
-        this.dataForChart7();
-        this.dataForChart8();
-        //this.dataForInfoBox3and4();
-    }
+   
     getData = (result) => {
         this.setState({rawData: result});
         this.firstChart();
         this.dataForChart1();
         this.dataForChart2();
         this.dataForChart3();
+        this.dataForChart7();
         this.dataForInfoBox1and2();
         this.dataForInfoBox3and4();
         console.log('This works');  
@@ -300,122 +263,30 @@ class Content extends React.Component {
         }
     }
 
-    dataForChart4 = () => {
-        // Left or Right Handed Batsmen
-        var handType = {};
-        if(Object.keys(this.state.rawPlayer).length !== 0){
-            handType.Left_Hand = 0;
-            handType.Right_Hand = 0;
-            this.state.rawPlayer.data.forEach(element => {
-                if(element.Batting_Hand === "Left_Hand" || element.Batting_Hand === "Right_Hand") {
-                    handType[element.Batting_Hand] += 1;
-                }
+   
+    dataForChart7 = () => {
+        // Season
+        let seasons = {};
+
+        if(Object.keys(this.state.rawData).length !== 0) {
+            this.state.rawData.data.forEach(element => {
+                if(seasons[element.season] !== undefined) {
+                    seasons[element.season]++;
+                } else
+                    seasons[element.season] = 1;
             });
+
+            
 
             this.setState({
-                chartData4: {
-                    labels: [...Object.keys(handType)],
+                chartData7: {
+                    labels: [...Object.keys(seasons)],
                     datasets: [
                         {
-                            label: 'Left or Right Handed Batsmen',
-                            data: [...Object.values(handType)],
+                            label: 'Average Runs Every Over',
+                            data: [...Object.values(seasons)],
                             backgroundColor: [
-                                'rgba(244, 67, 54, 0.5)',
-                                'rgba(9, 98, 234, 0.5)'
-                            ],
-                            borderColor: [
-                                'rgba(244, 67, 54,1)',
-                                'rgba(9, 98, 234, 1)'
-                            ],
-                            borderWidth: 1
-                        }
-                    ]
-                }
-            });
-            this.forceUpdate();
-        }
-    }
-
-    dataForChart5 = () => {
-        // Bowling Skill
-        var bowlingSkill = {};
-        if(Object.keys(this.state.rawPlayer).length !== 0){
-            this.state.rawPlayer.data.forEach(element => {
-                if(bowlingSkill[element.Bowling_Skill] !== undefined) {
-                    bowlingSkill[element.Bowling_Skill] += 1;
-                } else if(element.Bowling_Skill !== undefined && element.Bowling_Skill !== "NULL") {
-                    bowlingSkill[element.Bowling_Skill] = 1;
-                }
-            });
-
-            this.setState({
-                chartData5: {
-                    labels: [...Object.keys(bowlingSkill)],
-                    datasets: [
-                        {
-                            label: 'Bowling Skill',
-                            data: [...Object.values(bowlingSkill)],
-                            backgroundColor: [
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)',
-                                'rgba(0, 150, 136, 0.5)'
-                            ],
-                            borderColor: [
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)',
-                                'rgba(0, 150, 136, 1)'
-                            ],
-                            borderWidth: 1
-                        }
-                    ]
-                }
-            });
-            this.forceUpdate();
-        }
-    }
-
-    dataForChart6 = () => {
-        // Player Nationality
-        var nationality = {};
-        if(Object.keys(this.state.rawPlayer).length !== 0){
-            this.state.rawPlayer.data.forEach(element => {
-                if(nationality[element.Country] !== undefined) {
-                    nationality[element.Country] += 1;
-                } else if(element.Country !== undefined && element.Is_Umpire !== "1") {
-                    nationality[element.Country] = 1;
-                }
-            });
-
-            this.setState({
-                chartData6: {
-                    labels: [...Object.keys(nationality)],
-                    datasets: [
-                        {
-                            label: 'Player Nationality',
-                            data: [...Object.values(nationality)],
-                            backgroundColor: [
+                                'rgba(103, 58, 183, 0.5)',
                                 'rgba(255, 99, 132, 0.5)',
                                 'rgba(54, 162, 235, 0.5)',
                                 'rgba(245, 135, 31, 0.5)',
@@ -425,64 +296,6 @@ class Content extends React.Component {
                                 'rgba(255, 206, 86, 0.5)',
                                 'rgba(75, 192, 192, 0.5)',
                                 'rgba(153, 102, 255, 0.5)',
-                                'rgba(255, 159, 64, 0.5)',
-                                'rgba(255, 99, 132, 0.5)',
-                                'rgba(54, 162, 235, 0.5)',
-                                'rgba(245, 135, 31, 0.5)',
-                                'rgba(128, 203, 174, 0.5)'
-                            ],
-                            borderColor: [
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(245, 135, 31, 1)',
-                                'rgba(128, 203, 174, 1)',
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(255, 206, 86, 1)',
-                                'rgba(75, 192, 192, 1)',
-                                'rgba(153, 102, 255, 1)',
-                                'rgba(255, 159, 64, 1)',
-                                'rgba(255, 99, 132, 1)',
-                                'rgba(54, 162, 235, 1)',
-                                'rgba(245, 135, 31, 1)',
-                                'rgba(128, 203, 174, 1)'
-                            ],
-                            borderWidth: 1
-                        }
-                    ]
-                }
-            });
-            this.forceUpdate();
-        }
-    }
-
-    dataForChart7 = () => {
-        // Average runs per over
-        let averages = {};
-
-        if(Object.keys(this.state.rawBallByBall).length !== 0) {
-            this.state.rawBallByBall.data.forEach(element => {
-                if(averages[element.Over_Id] !== undefined && element.Batsman_Scored !== 'Do_nothing' && element.Batsman_Scored !== ' ') {
-                    averages[element.Over_Id] = { sum : averages[element.Over_Id].sum + element.Batsman_Scored, count : averages[element.Over_Id].count + 1 };
-                } else if(element.Over_Id !== undefined && element.Batsman_Scored !== 'Do_nothing' && element.Batsman_Scored !== ' ') {
-                    averages[element.Over_Id] = { sum : element.Batsman_Scored, count : 1 };
-                }
-            });
-
-            Object.entries(averages).forEach(element => {
-                console.log(element);
-                averages[element[0]] = parseFloat(element[1].sum) / element[1].count;
-            });
-
-            this.setState({
-                chartData7: {
-                    labels: [...Object.keys(averages)],
-                    datasets: [
-                        {
-                            label: 'Average Runs Every Over',
-                            data: [...Object.values(averages)],
-                            backgroundColor: [
-                                'rgba(103, 58, 183, 0.5)'
                             ],
                             borderColor: [
                                 'rgba(103, 58, 183,1)'
@@ -496,65 +309,7 @@ class Content extends React.Component {
         }
     }
 
-    dataForChart8 = () => {
-        // Types of Dismissals
-        var dismissalType = {};
-        if(Object.keys(this.state.rawBallByBall).length !== 0){
-            this.state.rawBallByBall.data.forEach(element => {
-                if(dismissalType[element.Dissimal_Type] !== undefined) {
-                    dismissalType[element.Dissimal_Type] += 1;
-                } else if(element.Dissimal_Type !== undefined && element.Dissimal_Type !== ' ') {
-                    dismissalType[element.Dissimal_Type] = 1;
-                }
-            });
-
-            this.setState({
-                chartData8: {
-                    labels: [...Object.keys(dismissalType)],
-                    datasets: [
-                        {
-                            label: 'Types of Dismissals',
-                            data: [...Object.values(dismissalType)],
-                            backgroundColor: [
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)',
-                                'rgba(233, 30, 99, 0.5)'
-                            ],
-                            borderColor: [
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)',
-                                'rgba(233, 30, 99, 1)'
-                            ],
-                            borderWidth: 1
-                        }
-                    ]
-                }
-            });
-            this.forceUpdate();
-        }
-    }
+    
 
     dataForInfoBox1and2 = () => {        
         // Number of Superovers
@@ -614,35 +369,32 @@ class Content extends React.Component {
             <div className='content-container'>
                 <div className='title-container'>
                     <h1><span className='bold'>The greatest show on Earth</span> - IPL</h1>
+
+                    <img src={image} alt='trophy'/>
                     <h2><span className='bold'>From Jumping Japaak to</span> - Ye das saal aapke naam</h2>
 
                 </div>
                 <div className='infobox-container'>
                     <InfoBox data={this.state.data1} title='Total Matches' icon={<i className="fas fa-cricket fa-4x"></i>} text='Matches played till now.' />
-                    <InfoBox data={this.state.data2} title='Total Superovers' icon={<i className="fas fa-cricket fa-4x"></i>} text='Number of tie matches till now.' />
+                    <InfoBox data={this.state.data2} title='Total Ties' icon={<i className="fas fa-cricket fa-4x"></i>} text='Number of ties' />
                     <InfoBox data={this.state.infoBox3} title='Field first wins' icon={<i className="fas fa-cricket fa-4x"></i>} text='Number of wins with field first'  />
                     <InfoBox data={this.state.infoBox4} title='Bat first wins' icon={<i className="fas fa-cricket fa-4x"></i>} text='Number of wins with bat first'  />
 
                 </div>
                 <div className='graph-container'>
                     <ChartDisplay 
-                        selectChart='line' 
-                        chartData={this.state.chartData7} 
-                        titleText='Average Runs Every Over' 
-                        selectLabel={true} 
-                        selectLabelText='Overs' 
-                    />
-                    <ChartDisplay 
-                        selectChart='bar' 
-                        displayLegend={false} 
-                        chartData={this.state.dataForChart} 
-                        titleText='STH THAT WORKS' 
-                    />
-                    <ChartDisplay 
                         selectChart='pie' 
-                        
+                        chartData={this.state.chartData7}   
+                        titleText='Yearwise distribution' 
+                        selectLabel={true} 
+                        selectLabelText='Year' 
+                    />
+                    
+                    <ChartDisplay 
+                        selectChart='horizontalBar' 
+                        displayLegend={false}
                         chartData={this.state.chartData1} 
-                        titleText='Winner team' 
+                        titleText='Wins' 
                     />
                     <ChartDisplay 
                         selectChart='horizontalBar' 
@@ -653,31 +405,10 @@ class Content extends React.Component {
                     <ChartDisplay 
                         selectChart='pie' 
                         chartData={this.state.chartData2} 
-                        titleText='Matches Hosted By Country' 
+                        titleText='Wins by bat/field first' 
                     />
-                    <ChartDisplay 
-                        selectChart='pie' 
-                        chartData={this.state.chartData4} 
-
-                        titleText='Left / Right Handed Batsmen' 
-                    />
-                    <ChartDisplay 
-                        selectChart='horizontalBar' 
-                        displayLegend={false} 
-                        chartData={this.state.chartData5} 
-                        titleText='Bowling Skill' 
-                    />
-                    <ChartDisplay 
-                        selectChart='horizontalBar' 
-                        displayLegend={false} 
-                        chartData={this.state.chartData6} 
-                        titleText='Player Nationality' 
-                    />
-                    <ChartDisplay 
-                        selectChart={x < 900 ? 'horizontalBar' : 'bar'} 
-                        displayLegend={false} chartData={this.state.chartData8} 
-                        titleText='Player Dismissal Types' 
-                    />
+                   
+                    
                 </div>
             </div>
         )
